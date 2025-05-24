9d557b89fa792a06d437ec6e8ecf4c5ce7fef3e7
Alex Smith <asmith62378@gmail.com>
2025-05-23 21:30:37 -0400

    Thinking the gpg sign issue is fixed

--- lib/src/elements/__tests__/Box.test.tsx
+++ lib/src/elements/__tests__/Box.test.tsx
@@ -25,10 +25,10 @@ describe('Box Component', () => {
         hasShadow={false}
         className="custom-class"
       >
-        Custom Box Test 5
+        Custom Box Test 6
       </Box>
     );
-    const boxElement = screen.getByText('Custom Box Test 5');
+    const boxElement = screen.getByText('Custom Box Test 6');
     expect(boxElement).toBeInTheDocument();
     expect(boxElement).toHaveClass('box');
     expect(boxElement).toHaveClass('p-3');
