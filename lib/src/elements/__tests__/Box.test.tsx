c7991c484379c0a61d9317d52ec535326ebbcc32
Alex Smith <asmith62378@gmail.com>
2025-05-23 21:26:16 -0400

    Test new gpg key

--- lib/src/elements/__tests__/Box.test.tsx
+++ lib/src/elements/__tests__/Box.test.tsx
@@ -25,10 +25,10 @@ describe('Box Component', () => {
         hasShadow={false}
         className="custom-class"
       >
-        Custom Box Test 4
+        Custom Box Test 5
       </Box>
     );
-    const boxElement = screen.getByText('Custom Box Test 4');
+    const boxElement = screen.getByText('Custom Box Test 5');
     expect(boxElement).toBeInTheDocument();
     expect(boxElement).toHaveClass('box');
     expect(boxElement).toHaveClass('p-3');
