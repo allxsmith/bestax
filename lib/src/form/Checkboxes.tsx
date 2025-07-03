c09959d20c03440a0d99b848d80f6ebff1a993c2
Alex Smith <asmith62378@gmail.com>
2025-07-02 20:28:03 -0400

    Refactored form components to use internal classNames

--- lib/src/form/Checkboxes.tsx
+++ lib/src/form/Checkboxes.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import classNames from 'classnames';
+import classNames from '../helpers/classNames';
 import { useBulmaClasses, BulmaClassesProps } from '../helpers/useBulmaClasses';
 
 export interface CheckboxesProps extends Omit<BulmaClassesProps, 'color'> {
