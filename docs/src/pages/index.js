f15ef1ceb37050af61632aa690c6d61b78db5e5e
Alex Smith <asmith62378@gmail.com>
2025-07-15 21:58:44 -0400

    Update meta description for sharing

--- docs/src/pages/index.js
+++ docs/src/pages/index.js
@@ -33,8 +33,8 @@ export default function Home() {
   const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />"
+      title={`Welcome to ${siteConfig.title}`}
+      description="Build awesome websites, with Bulma and React"
     >
       <HomepageHeader />
       <main>
