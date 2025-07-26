de5b216e4c738618cb5fca4de2106c874f8ac82d
Alex Smith <asmith62378@gmail.com>
2025-07-26 01:04:30 -0400

    docs: add copy button to BrowserWindow
    To resolve issue #44, I added a copy button to the BrowserWindow component
    which serves as a frame or decoration around the LiveEditor

---
+++ docs/src/theme/CodeBlock/CustomCopyButton/index.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import clsx from 'clsx';
+import Translate from '@docusaurus/Translate';
+import Button from '@theme/CodeBlock/Buttons/Button';
+import IconCopy from '@theme/Icon/Copy';
+import IconSuccess from '@theme/Icon/Success';
+
+import styles from './styles.module.css';
+
+function CustomCopyButton({ code, className }) {
+  const [isCopied, setIsCopied] = useState(false);
+
+  const handleCopy = async () => {
+    await navigator.clipboard.writeText(code);
+    setIsCopied(true);
+    setTimeout(() => setIsCopied(false), 2000);
+  };
+
+  return (
+    <Button
+      type="button"
+      aria-label={isCopied ? 'Copied' : 'Copy code to clipboard'}
+      title="Copy"
+      className={clsx(
+        className,
+        styles.copyButton,
+        isCopied && styles.copyButtonCopied
+      )}
+      onClick={handleCopy}
+    >
+      <span className={styles.copyButtonIcons} aria-hidden="true">
+        <IconCopy className={styles.copyButtonIcon} />
+        <IconSuccess className={styles.copyButtonSuccessIcon} />
+      </span>
+    </Button>
+  );
+}
+
+export default CustomCopyButton;
